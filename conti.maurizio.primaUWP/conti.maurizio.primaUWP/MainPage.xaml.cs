15afa478264889f1e8059c68090fc8233b12c49f
Davide <davi98de@gmail.com>
2016-12-22 14:24:51 +0100

    Cambio Timer con await e rimossi using non usati

--- conti.maurizio.primaUWP/conti.maurizio.primaUWP/MainPage.xaml.cs
+++ conti.maurizio.primaUWP/conti.maurizio.primaUWP/MainPage.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System;
 using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
 
@@ -23,30 +13,32 @@ namespace conti.maurizio.primaUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        DispatcherTimer timer = new DispatcherTimer();
+        private const string _webViewUrl = "https://freeboard.io/board/Zo0rcA";
 
         public MainPage()
         {
             this.InitializeComponent();
-            timer.Interval = TimeSpan.FromMilliseconds(200);
-            timer.Tick += Timer_Tick;
-            timer.Start();
         }
 
-        private void Timer_Tick(object sender, object e)
+        private async Task SetSliderValue(double value)
         {
-            timer.Stop();
-
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("https://dweet.io");
-            client.GetStringAsync($"dweet/for/itts5h?slider={sliderValore.Value}");
-
-            webView.Navigate(new Uri("https://freeboard.io/board/Zo0rcA"));
+            await client.GetStringAsync($"dweet/for/itts5h?slider={sliderValore.Value}");
         }
 
-        private void sliderValore_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        private async void sliderValore_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-            timer.Start();
+            await Task.Delay(TimeSpan.FromMilliseconds(200));
+            await this.SetSliderValue(e.NewValue);
+        }
+
+        /// <summary>
+        /// Al caricamento completo della pagina
+        /// </summary>
+        private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            webView.Navigate(new Uri(_webViewUrl));
         }
     }
 }
